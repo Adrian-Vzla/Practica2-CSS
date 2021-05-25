3aa80c85a27c2d4fa4e6601ca6a52b77abc5576e
Adrian-Vzla <adrianvalenzuela99@gmail.com>
2021-05-24 22:44:58 -0700

    Creacion de los Heroes(Cards)

--- SPA/src/app/components/heroes/heroes.component.ts
+++ SPA/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, HeroesService } from 'src/app/servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
+  heroes: Array <Heroe>=[]
+  constructor(private servicioHeroes: HeroesService){
+    this.heroes = this.servicioHeroes.getHeroes();
+   }
 
   ngOnInit(): void {
   }
