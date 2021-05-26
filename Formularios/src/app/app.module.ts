40c5eb3c935de714fb35506a48721ba85df29fe4
Adrian-Vzla <adrianvalenzuela99@gmail.com>
2021-05-25 19:46:11 -0700

    Cambios para poner Nombres Predeterminados (info)

--- Formularios/src/app/app.module.ts
+++ Formularios/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule} from './app.routing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './pages/template/template.component';
 import { ReactiveComponent } from './pages/reactive/reactive.component';
@@ -14,7 +15,8 @@ import { ReactiveComponent } from './pages/reactive/reactive.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
